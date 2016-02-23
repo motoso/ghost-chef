99fb1d787e97321a3be93a358417593b0dd75dda
motoso <motosonomoto@gmail.com>
2016-02-24 02:41:16 +0900

    metadata.rbを加筆

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'ghost-chef'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Server settings for ghost'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
